4eb81d86abd0aa0faec8ab61e681fd5973ad4a51
Baodi Shi <baodi@apache.org>
2023-12-25 19:16:38 +0800

    back

--- server.js
+++ server.js
@@ -1,52 +1,59 @@
 const express = require('express');
 const WebSocket = require('ws');
 const pty = require('node-pty');
-const os = require('os');
 
 // 初始化 Express 和 WebSocket 服务器
 const app = express();
-const wss = new WebSocket.Server({noServer: true});
+const wss = new WebSocket.Server({ noServer: true });
 
-// 创建一个新的 pty
-const shell = os.platform() === 'win32' ? 'powershell.exe' : 'zsh';
-const ptyProcess = pty.spawn(shell, [], {
-    name: 'xterm-color',
-    cols: 80,
-    rows: 30,
-    cwd: process.env.HOME,
-    env: process.env
-});
+let userInput = '';
 
-// 在 pty 中执行代理命令和 apiserver-admin 命令
-ptyProcess.write('proxy\n');
-ptyProcess.write('apiserver-admin pools connect aws-use2-dixie-snc -n streamnative\n');
+wss.on('connection', (ws) => {
+    let echoShell = pty.spawn('zsh', ['-i'], {
+        name: 'xterm-color',
+        cwd: process.env.HOME,
+        env: process.env
+    });
 
-// 当 WebSocket 接收到消息时，将其写入 pty
-wss.on('connection', ws => {
-    ws.on('message', message => {
-        // 检查消息是否是一个字符串
-        let stringMessage;
-        if (typeof message !== 'string') {
-            stringMessage = message.toString();
-        }
-        // 检查是否为不允许的命令
-        if (stringMessage.trim().startsWith('kubectl delete')) {
-            ws.send('Error: kubectl delete command is not allowed.\n');
+    let execShell = pty.spawn('zsh', ['-i'], {
+        name: 'xterm-color',
+        cwd: process.env.HOME,
+        env: process.env
+    });
+
+    ws.on('message', (message) => {
+        userInput += message;
+
+        console.log(message + ':' + userInput)
+
+        if (message.includes('\r')) { // 检测 Enter 键
+            if (userInput.trim() === 'delete') {
+                ws.send('\r\nError: The "delete" command is not allowed.\r\n');
+                userInput = '';
+            } else {
+                execShell.write(userInput);
+                userInput = '';
+            }
         } else {
-            // 如果消息不能被解析为 JSON，将其作为一个普通的命令处理
-            ptyProcess.write(stringMessage + '\n');
+            echoShell.write(message); // 立即回显字符
         }
     });
-});
 
 
+    echoShell.on('data', (data) => {
+        ws.send(data);
+    });
 
-// 当 pty 接收到数据时，将其发送到所有 WebSocket 连接
-ptyProcess.on('data', data => {
-    wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
+    execShell.on('data', (data) => {
+        ws.send(data);
+    });
+
+    echoShell.on('exit', () => {
+        ws.close();
+    });
+
+    execShell.on('exit', () => {
+        ws.close();
     });
 });
 
